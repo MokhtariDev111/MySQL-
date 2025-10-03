1233cb2fe8e6d6a8ebac0c40de0360dd4fa864d7
Mohamed Aziz Mokhtari <mohmedazizmokhtari@gmail.com>
2025-10-03 13:22:35 +0100

    Update sales.sql

--- sales.sql
+++ sales.sql
@@ -1,141 +1,204 @@
+#be careful about the column name : custmer/customer
+
 USE sales;
 
-#List all customers in the customers table:
-SELECT DISTINCT custmer_name FROM sales.customers;
+-- List all customers in the customers table
+SELECT DISTINCT customer_name 
+FROM sales.customers;
 
-#Retrieve all products with their corresponding customer IDs.
-select T.customer_code , P.Product_code,P.Product_type
-from Transactions T 
-inner join Products P 
-on T.product_code=P.Product_code
-order by T.customer_code;
+-- Retrieve all products with their corresponding customer IDs
+SELECT T.customer_code, P.Product_code, P.Product_type
+FROM Transactions T
+INNER JOIN Products P 
+    ON T.product_code = P.Product_code
+ORDER BY T.customer_code;
 
-#Show all markets in the markets table located in the South region.
-select * from markets where LOWER(zone) ='south';
+-- Show all markets in the South region
+SELECT * 
+FROM markets 
+WHERE LOWER(zone) = 'south';
 
-#Select all transactions where the quantity is greater than 50.
-select * from transactions where sales_qty>50;
+-- Select all transactions where the quantity is greater than 50
+SELECT * 
+FROM transactions 
+WHERE sales_qty > 50;
 
+-- Show columns from transactions
 SHOW COLUMNS FROM transactions;
 
-#Retrieve all products sold after 2018-01-01.
-select * from transactions where order_date>'2018-01-01' order by order_date;
+-- Retrieve all products sold after 2018-01-01
+SELECT * 
+FROM transactions 
+WHERE order_date > '2018-01-01' 
+ORDER BY order_date;
 
-#Show customers whose name contains Stores.
-select * from customers where custmer_name like '%stores%';
+-- Show customers whose name contains "Stores"
+SELECT * 
+FROM customers 
+WHERE customer_name LIKE '%stores%';
 
+-- Rename column in customers table
 ALTER TABLE customers
 RENAME COLUMN custmer_name TO customer_name;
 
-#List transactions in descending order of price.
-select *  from transactions order by sales_amount desc ; 
+-- List transactions in descending order of price
+SELECT *  
+FROM transactions 
+ORDER BY sales_amount DESC;
 
-#Find all markets not in the Central region.
-select * from markets where zone not in ('Central') ; 
+-- Find all markets not in the Central region
+SELECT * 
+FROM markets 
+WHERE zone NOT IN ('Central');
 
-#Count the number of products sold by each customer.
-select customer_code , count(product_code) as  number_of_products   from transactions group by customer_code order by number_of_products ;
+-- Count the number of products sold by each customer
+SELECT customer_code, COUNT(product_code) AS number_of_products
+FROM transactions
+GROUP BY customer_code
+ORDER BY number_of_products;
 
-#Calculate the total quantity sold for each product.
-select product_code , sum(sales_qty) as  total_qty_by_product   from transactions group by product_code order by total_qty_by_product  ;
+-- Calculate the total quantity sold for each product
+SELECT product_code, SUM(sales_qty) AS total_qty_by_product
+FROM transactions
+GROUP BY product_code
+ORDER BY total_qty_by_product;
 
-#average sales_amount per market.
-select market_code, round(avg(sales_amount),3) as  avg_price  from transactions group by market_code order by avg_price ;
+-- Average sales_amount per market
+SELECT market_code, ROUND(AVG(sales_amount), 3) AS avg_price
+FROM transactions
+GROUP BY market_code
+ORDER BY avg_price;
 
-#List the number of transactions per year.
-select year(order_date) as years , count(*) as number_of_transaction from transactions group by  year(order_date) ;
+-- List the number of transactions per year
+SELECT YEAR(order_date) AS years, COUNT(*) AS number_of_transaction
+FROM transactions
+GROUP BY YEAR(order_date);
 
-#List all products along with their market name.
-select T.product_code , M.markets_name 
-from Transactions T 
-inner join markets M 
-on T.market_code=M.markets_code;
- 
-#Show all transactions with customer name, product, and market region.
-select customer_name , product_type ,zone
-from Transactions T 
-join Customers C 
-on T.customer_code=C.Customer_code
-join Products P 
-on T.Product_code = P.Product_code
-join Markets M
-on T.market_code=M.markets_code;
+-- List all products along with their market name
+SELECT T.product_code, M.markets_name
+FROM Transactions T
+INNER JOIN markets M 
+    ON T.market_code = M.markets_code;
 
-#return customers that placed orders 
-#we can do it with inner join or with exist
-#methode 1 
+-- Show all transactions with customer name, product, and market region
+SELECT C.customer_name, P.product_type, M.zone
+FROM Transactions T
+JOIN Customers C 
+    ON T.customer_code = C.Customer_code
+JOIN Products P 
+    ON T.Product_code = P.Product_code
+JOIN Markets M 
+    ON T.market_code = M.markets_code;
+
+-- Return customers that placed orders
+-- Method 1: INNER JOIN
 SELECT DISTINCT C.*
 FROM Customers C
 INNER JOIN Transactions T
-  ON C.Customer_code = T.Customer_code;
-# methode 2 
-select * from customers where exists (select * from transactions where  customers.Customer_code=transactions.Customer_code);
+    ON C.Customer_code = T.Customer_code;
 
+-- Method 2: EXISTS
+SELECT * 
+FROM customers 
+WHERE EXISTS (
+    SELECT 1 
+    FROM transactions 
+    WHERE customers.Customer_code = transactions.Customer_code
+);
 
-#List customers who have never bought Prod002.
-#methode 1 
-SELECT 
-    Customer_code,
-    GROUP_CONCAT(DISTINCT Product_code ORDER BY Product_code) AS Products_Bought
+-- List customers who have never bought Prod002
+-- Method 1: Using GROUP_CONCAT
+SELECT Customer_code,
+       GROUP_CONCAT(DISTINCT Product_code ORDER BY Product_code) AS Products_Bought
 FROM Transactions
 GROUP BY Customer_code
 HAVING NOT FIND_IN_SET('Prod002', Products_Bought);
 
-#methode 2
-select customer_code from transactions where product_code not in ('Prod002') ;
-#wrong becasue  If a customer bought Prod002 once and another product in another row, the row of the customer_code will still appear bc where clasue check row by row , each transactions individually 
-#it does not check the full set of transactions 
-#so: 
+-- Method 2: Using NOT EXISTS
 SELECT Customer_code
 FROM Customers C
 WHERE NOT EXISTS (
-    SELECT 1 #it doesnt matter the value selected , the exist checks if at least one row exists and  in this case it chekcs the condition across all transactions of a customer
+    SELECT 1
     FROM Transactions T
     WHERE T.Customer_code = C.Customer_code
       AND T.Product_code = 'Prod002'
 );
-#It does not stop at the first row that fails; it looks for at least one matching row anywhere in the customer’s transactions.
 
-#Show the total amount spent by each customer (price × quantity).
-select customer_code , sum(sales_qty*sales_amount) as total_amount_spent from transactions group by customer_code;
+-- Show the total amount spent by each customer
+SELECT customer_code, SUM(sales_amount) AS total_amount_spent
+FROM transactions
+GROUP BY customer_code;
 
+-- Find the product(s) with the highest price
+SELECT product_code, sales_amount
+FROM transactions
+ORDER BY sales_amount DESC
+LIMIT 1;
 
-#Find the product(s) with the highest price.
-select product_code ,sales_amount from transactions order by sales_amount desc limit 1 ; 
+-- List customers who have made transactions in Central
+SELECT customer_code, market_code
+FROM transactions
+WHERE market_code IN (
+    SELECT market_code 
+    FROM markets 
+    WHERE LOWER(zone) = 'central'
+)
+GROUP BY customer_code, market_code;
 
-#List customers who have made transactions in Central.
-select customer_code,market_code  from transactions where market_code in (select market_code from markets where Lower(zone)='Central') group by customer_code,market_code;
+-- Retrieve markets where total sales exceeded 10,000,000,000
+SELECT market_code, SUM(sales_amount) AS total_sales
+FROM transactions
+GROUP BY market_code
+HAVING SUM(sales_amount) > 10000000000;
 
+-- Find products that have never been sold
+-- Method 1: NOT EXISTS
+SELECT product_code 
+FROM products 
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM transactions 
+    WHERE products.product_code = transactions.product_code
+);
 
-#Retrieve markets where total sales quantity exceeded 10000000000.
-select  market_code,sum(sales_amount*sales_qty) as total_sales from transactions group by market_code  having total_sales > 10000000000;
+-- Method 2: NOT IN
+SELECT product_code 
+FROM products 
+WHERE product_code NOT IN (
+    SELECT product_code 
+    FROM transactions
+);
 
+-- Show monthly sales totals for each product
+SELECT product_code, MONTH(order_date) AS monthly, SUM(sales_amount) AS total
+FROM transactions
+GROUP BY MONTH(order_date), product_code
+ORDER BY monthly, product_code;
 
-#Find products that have never been sold.
-select product_code from products where not exists(select * from transactions where products.product_code=transactions.product_code);#M1
-select product_code from products where product_code not in  (select product_code  from transactions );#M2
+-- Show average transaction price per customer and rank them
+SELECT customer_code,
+       AVG(sales_amount) AS avg_transaction_price,
+       DENSE_RANK() OVER(ORDER BY AVG(sales_amount) DESC) AS rank_by_avg
+FROM transactions
+GROUP BY customer_code;
 
+-- How many unique products bought by each customer
+SELECT customer_code, COUNT(DISTINCT product_code) AS number_unique_products
+FROM transactions
+GROUP BY customer_code
+ORDER BY number_unique_products ASC;
 
-#Show monthly sales totals for each product. 
-select min(order_date) day1 , max(order_date) as last_date from transactions; #quick check
-select product_code,month(order_date) as monthly , sum(sales_amount*sales_qty) as total from transactions group by monthly,product_code order by monthly ,product_code;
+-- Customers who bought products from more than one market
+SELECT customer_code, COUNT(DISTINCT market_code) AS num_markets
+FROM transactions
+GROUP BY customer_code
+HAVING num_markets > 1
+ORDER BY customer_code;
 
-#Show average transaction price per customer and rank them. 
-select
- customer_code , avg(sales_amount*sales_qty) as average , dense_rank() over(order by avg(sales_amount*sales_qty)  desc) as rank_by_avg 
- from transactions group by customer_code ;
-
-#how many products bought by each customer
-select customer_code , count(distinct product_code)as number_unique_products from transactions group by customer_code  order by number_unique_products asc;
-
-#customers who bought products from more than one market.
-select customer_code ,count(distinct market_code) num_markets from transactions group by customer_code having num_markets > 1 order by customer_code;
-
-#the trend of sales per region over the years.
-select year(order_date) as years , zone, sum(sales_amount*sales_qty) as total_sales
-from Transactions t 
-join markets M 
-on T.market_code=M.Markets_code
-group by years,zone
-order by years;
- 
+-- Trend of sales per region over the years
+SELECT YEAR(t.order_date) AS years, M.zone, SUM(t.sales_amount) AS total_sales
+FROM Transactions t
+JOIN markets M 
+    ON t.market_code = M.markets_code
+GROUP BY years, M.zone
+ORDER BY years;
